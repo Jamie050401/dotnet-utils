10c6239c7d13d8218feb133ce91102096ba0036a
Jamie050401 <jamieallen050401@gmail.com>
2024-05-23 16:34:04 +0100

    Wrapped System.Collections.Generic.Dictionary and System.Collections.Generic.KeyValuePair for more idiomatic use in F#

---
+++ FSharpUtils/Dictionary.fs
@@ -0,0 +1,16 @@
+﻿namespace Utils.FSharp
+
+type Dictionary<'TKey, 'TValue> = System.Collections.Generic.Dictionary<'TKey, 'TValue>
+
+module Dictionary =
+    let add key value (dictionary: Dictionary<'TKey, 'TValue>) =
+        dictionary.Add (key, value)
+
+    let remove key (dictionary: Dictionary<'TKey, 'TValue>) =
+        dictionary.Remove key
+
+    let exists key (dictionary: Dictionary<'TKey, 'TValue>) =
+        dictionary.ContainsKey key
+
+    let create (data: KeyValuePair<'TKey, 'TValue> seq) =
+        new Dictionary<'TKey, 'TValue> (data)
---
+++ FSharpUtils/KeyValuePair.fs
@@ -0,0 +1,13 @@
+﻿namespace Utils.FSharp
+
+type KeyValuePair<'TKey, 'TValue> = System.Collections.Generic.KeyValuePair<'TKey, 'TValue>
+
+module KeyValuePair =
+    let create (key: 'TKey) (value: 'TValue) =
+        System.Collections.Generic.KeyValuePair.Create (key, value)
+
+    let ofTuple (key: 'TKey, value: 'TValue) =
+        create key value
+
+    let toTuple (keyValuePair: KeyValuePair<'TKey, 'TValue>) =
+        keyValuePair.Key, keyValuePair.Value
