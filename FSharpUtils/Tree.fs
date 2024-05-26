8adcd0d9e5a7b253cf962ecb3d1bebde012c4708
Jamie050401 <jamieallen050401@gmail.com>
2024-05-26 19:55:02 +0100

    Implemented the beginnings of a Tree type

---
+++ FSharpUtils/Tree.fs
@@ -0,0 +1,21 @@
+﻿namespace Utils.FSharp
+
+type Node<'T> = {
+    Value: 'T
+}
+
+type Branch<'T> =
+    | Branch of Node<'T> * Branch<'T> array
+    | Node of Node<'T>
+
+module Tree =
+    // TODO: Needs unit tests
+    let rec iter func tree =
+        tree |> Array.iter (fun branch ->
+            match branch with
+            | Node node ->
+                func node
+            | Branch (node, tree) ->
+                func node
+                tree |> iter func
+        )
