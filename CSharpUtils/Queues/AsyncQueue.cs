483734835d1b3898a67b717ab5be39577f61dff7
Jamie050401 <jamieallen050401@gmail.com>
2024-08-11 17:54:24 +0100

    Updated AsyncQueue

--- CSharpUtils/Queues/AsyncQueue.cs
+++ CSharpUtils/Queues/AsyncQueue.cs
@@ -5,19 +5,19 @@ namespace Utils.CSharp.Queues;
 
 public class AsyncQueue<T>
 {
-    private AsyncQueue(Action<T> process, int threadCount = 1)
+    private AsyncQueue(Action<T> action, int threadCount = 1)
     {
         if (threadCount < 1)
             throw new ArgumentException($"{nameof(threadCount)} cannot be less than one.");
 
-        Process = process;
+        Action = action;
         ThreadCount = threadCount;
 
         ProcessingSemaphore = new(threadCount);
 
         Enumerable
             .Range(1, threadCount)
-            .ForEach(_ => Task.Run(() => Enumerate(process), Token));
+            .ForEach(_ => Enumerate(action));
     }
 
     public static AsyncQueue<Action> Create(int threadCount = 1) => new(action => action(), threadCount);
@@ -39,7 +39,7 @@ public class AsyncQueue<T>
 
         Enumerable
             .Range(1, ThreadCount)
-            .ForEach(_ => Task.Run(() => Enumerate(Process), Token));
+            .ForEach(_ => Enumerate(Action));
     }
 
     public void Stop()
@@ -47,37 +47,42 @@ public class AsyncQueue<T>
         CancellationTokenSource.CreateLinkedTokenSource(Token).Cancel();
     }
 
-    private async Task Enumerate(Action<T> process)
+    private void Enumerate(Action<T> action)
     {
-        await ProcessingSemaphore.WaitAsync();
-
-        try
+        async Task Enumerate(Action<T> action)
         {
-            while (true)
+            await ProcessingSemaphore.WaitAsync();
+
+            try
             {
-                EmptyWaitHandle.Wait();
-
-                if (Queue.TryDequeue(out var item))
+                while (true)
                 {
-                    process(item);
+                    EmptyWaitHandle.Wait();
 
-                    if (IsEmpty)
+                    if (Queue.TryDequeue(out var item))
                     {
-                        EmptyWaitHandle.Reset();
+                        action(item);
+
+                        if (IsEmpty)
+                        {
+                            EmptyWaitHandle.Reset();
+                        }
                     }
                 }
             }
+            finally
+            {
+                ProcessingSemaphore.Release();
+            }
         }
-        finally
-        {
-            ProcessingSemaphore.Release();
-        }
+
+        Task.Run(() => Enumerate(action), Token);
     }
 
     public bool IsEmpty { get => Queue.IsEmpty; }
     private CancellationToken Token { get; set; } = new();
     private ConcurrentQueue<T> Queue { get; } = new();
-    private Action<T> Process { get; }
+    private Action<T> Action { get; } // This action defines the logic to be applied to each element in the queue.
     private SemaphoreSlim ProcessingSemaphore { get; } // This semaphore is used to restrict the number of threads that can be processing the queue at any given moment.
     private ManualResetEventSlim EmptyWaitHandle { get; } = new(); // This manual reset event is used to halt the processing loop whenever the queue is empty.
     private int ThreadCount { get; }
